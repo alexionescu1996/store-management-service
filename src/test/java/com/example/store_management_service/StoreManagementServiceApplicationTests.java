f07d2aff29b48adbca9b401091d5cf79899407b3
Alex_I <alex_ionescu96@yahoo.com>
2024-08-01 10:21:27 +0300

    init

---
+++ src/test/java/com/example/store_management_service/StoreManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.store_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
