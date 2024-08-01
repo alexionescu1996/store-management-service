1fff5e4bb22b0d9d8c08d13c7f33077f60f34a4e
Alex_I <alex_ionescu96@yahoo.com>
2024-08-01 15:19:12 +0300

    packages restructure

---
+++ src/main/java/com/example/demo/dao/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.dao;
+
+import com.example.demo.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
+
