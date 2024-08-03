9eba5472c84b98da0a299ebc98f84a3231616b68
Alex_I <alex_ionescu96@yahoo.com>
2024-08-03 13:21:12 +0300

    added global exception handler

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.example.demo.exception;
 
+import com.example.demo.aspect.LoggingAspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,9 +11,19 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(ProductNotFoundException.class)
-    public ResponseEntity<?> handleProductNotFoundException(ProductNotFoundException e) {
+    public ResponseEntity<ErrorDetails> handleProductNotFoundException(ProductNotFoundException e) {
         ErrorDetails errorDetails = new ErrorDetails(e.getMessage());
+        logger.error("Product not found exception: {}", e.getMessage(), e);
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> handleGlobalException(Exception e) {
+        ErrorDetails errorDetails = new ErrorDetails(e.getMessage());
+        logger.error("An error occurred: {}", e.getMessage(), e);
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
